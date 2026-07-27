a8fe4a54b5cdcac28073408bd400e80b7948e374
sthiti321 <sthitipragyanp2@gmail.com>
2026-07-27 20:55:19 +0530

    Auto-create database tables and admin user

--- app.py
+++ app.py
@@ -556,10 +556,19 @@ def generate_report():
 # ------------------------
 
 
+# Create database tables
+with app.app_context():
+    db.create_all()
+
+    if not User.query.filter_by(username="admin").first():
+        admin = User(
+            username="admin",
+            password="admin123",
+            role="Administrator"
+        )
+        db.session.add(admin)
+        db.session.commit()
+
+# Run locally
 if __name__ == "__main__":
-
-    with app.app_context():
-
-        db.create_all()
-
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
